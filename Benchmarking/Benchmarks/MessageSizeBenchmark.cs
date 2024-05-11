a201c01a0b4afb53033d64a254c0d6983c73cbe7
Vaclav Bily <xbily@fi.muni.cz>
2024-05-11 16:09:28 +0200

    MessageSizeBenchmark - added RestApi baseline

--- Benchmarking/Benchmarks/MessageSizeBenchmark.cs
+++ Benchmarking/Benchmarks/MessageSizeBenchmark.cs
@@ -1,4 +1,5 @@
-﻿using Messaging.RabbitMQ;
+﻿using System.Net.Http.Json;
+using Messaging.RabbitMQ;
 using RabbitMQ.Client.Events;
 
 namespace Benchmarking.Benchmarks;
@@ -13,6 +14,10 @@ public class MessageSizeBenchmark
     private RabbitTestingHelper _sendTestingHelper = null!;
     private RabbitTestingHelper _receiveTestingHelper = null!;
 
+    private const string RestApiBaseUrl = "http://localhost:5050";
+
+    private HttpClient _httpClient = null!;
+
     // 512B, 1KB, 512KB, 1MB, 8MB, 32MB, 64 MB, 128MB (max message size)
     [Params(512, 1024, 1024 * 512, 1_048_576, 8_388_608, 33_554_432, 67_108_864, 134_217_728)]
     public int MessageSize;
@@ -26,13 +31,15 @@ public class MessageSizeBenchmark
         _message = new byte[MessageSize];
         _sendTestingHelper = new RabbitTestingHelper(SendQueue);
         _receiveTestingHelper = new RabbitTestingHelper(ReceiveQueue, onMessageReceived: OnConsumerOnReceived);
+        _httpClient = PrepareHttpClient(RestApiBaseUrl);
     }
 
     [GlobalCleanup]
     public void Cleanup()
     {
-        _sendTestingHelper.Dispose();
-        _receiveTestingHelper.Dispose();
+        _sendTestingHelper?.Dispose();
+        _receiveTestingHelper?.Dispose();
+        _httpClient?.Dispose();
     }
 
     [IterationSetup]
@@ -41,8 +48,12 @@ public class MessageSizeBenchmark
         _messageReceivedSource = new TaskCompletionSource<bool>();
     }
 
+    [Benchmark(Baseline = true)]
+    public async Task<HttpResponseMessage> MessageRestApi() =>
+        await _httpClient.PostAsJsonAsync("receive", new { Data = _message });
+
     [Benchmark]
-    public async Task MessageWaitForReply()
+    public async Task MessageRabbitWaitForReply()
     {
         _sendTestingHelper.SendMessage(_message);
 
@@ -54,5 +65,13 @@ public class MessageSizeBenchmark
     private void OnConsumerOnReceived(object? model, BasicDeliverEventArgs ea)
     {
         _messageReceivedSource.SetResult(true);
+    }
+
+    private static HttpClient PrepareHttpClient(string apiBaseUrl)
+    {
+        var client = new HttpClient(new HttpClientHandler());
+        client.BaseAddress = new Uri(apiBaseUrl);
+        client.Timeout = TimeSpan.FromMinutes(5);
+        return client;
     }
 }
