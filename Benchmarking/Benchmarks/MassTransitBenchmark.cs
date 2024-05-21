2312c83d255bc7ffff79200ee13af2efc2b73e23
Vaclav Bily <xbily@fi.muni.cz>
2024-05-21 12:28:23 +0200

    MassTransitBenchmark added Rabbit Client for comparison

--- Benchmarking/Benchmarks/MassTransitBenchmark.cs
+++ Benchmarking/Benchmarks/MassTransitBenchmark.cs
@@ -1,17 +1,28 @@
-﻿using Messaging.RabbitMQ;
+﻿using BenchmarkDotNet.Engines;
+using Messaging.RabbitMQ;
 using Messaging.RabbitMQ.MassTransit;
+using RabbitMQ.Client.Events;
 
 namespace Benchmarking.Benchmarks;
 
 [MarkdownExporter]
 [MinColumn, MaxColumn, MeanColumn, MedianColumn]
+[SimpleJob(RunStrategy.Throughput, warmupCount: 100)]
 public class MassTransitBenchmark
 {
     private MassTransitHelper _massTransitHelper = null!;
 
+    private RabbitTestingHelper _sendTestingHelper = null!;
+    private RabbitTestingHelper _receiveTestingHelper = null!;
+    private TaskCompletionSource<bool> _rabbitMessageReceivedSource = null!;
+
     [GlobalSetup]
     public void Setup()
     {
+        _sendTestingHelper = new RabbitTestingHelper(RabbitConstants.SendQueue);
+        _receiveTestingHelper =
+            new RabbitTestingHelper(RabbitConstants.SendQueue, onMessageReceived: OnConsumerOnReceived);
+
         _massTransitHelper = new MassTransitHelper();
         _massTransitHelper.Start();
     }
@@ -20,6 +31,8 @@ public class MassTransitBenchmark
     public void Cleanup()
     {
         _massTransitHelper?.Dispose();
+        _sendTestingHelper?.Dispose();
+        _receiveTestingHelper?.Dispose();
     }
 
     [IterationSetup]
@@ -29,10 +42,25 @@ public class MassTransitBenchmark
     }
 
 
-    [Benchmark(Description = "MassTransit", OperationsPerInvoke = 100000)]
+    [Benchmark(Baseline = true, Description = "RabbitClient", OperationsPerInvoke = 100)]
+    public async Task MessageRabbitWaitForReply()
+    {
+        _rabbitMessageReceivedSource = new TaskCompletionSource<bool>(); // Reset the source
+        _sendTestingHelper.SendMessage("Test"u8.ToArray());
+
+        // Wait for the reply
+        await _rabbitMessageReceivedSource.Task
+            .ConfigureAwait(false);
+    }
+
+    [Benchmark(Description = "MassTransit", OperationsPerInvoke = 100)]
     public async Task PublishMessage()
     {
         await _massTransitHelper.PublishMessage(new TestRequest("Test"));
-        await _massTransitHelper.Consumer.WaitForMessage();
+        await _massTransitHelper.Consumer.WaitForMessage()
+            .ConfigureAwait(false);
     }
+
+    private void OnConsumerOnReceived(object? model, BasicDeliverEventArgs ea) =>
+        _rabbitMessageReceivedSource?.SetResult(true);
 }
